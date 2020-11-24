40556dead738fc8300e62f9279c693b45632c015
kat2804 <60763154+kat2804@users.noreply.github.com>
2020-11-23 22:41:31 -0300

    terceiro commit

--- app/src/main/java/br/unigran/alunos/MainActivity.java
+++ app/src/main/java/br/unigran/alunos/MainActivity.java
@@ -1,15 +1,17 @@
 package br.unigran.alunos;
-
+import com.google.firebase.analytics.FirebaseAnalytics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
     }
 }
